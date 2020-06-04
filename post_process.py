f9f5e41f60a4fe53164292bc4f8df41d45665ad6
caozidong <42765654+caozidong@users.noreply.github.com>
2020-06-04 20:42:33 +0800

    Update post_process.py

--- post_process.py
+++ post_process.py
@@ -1,6 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from SAD_matching import SAD_2
 import cv2
 from PIL import Image
 import time
@@ -14,54 +13,9 @@ def LRC(input_l, input_r, thres=20):
         for j in range(w):
             if abs(int(input_l[i, j]) - int(input_r[i, j-input_l[i, j]])) < thres and (j-input_l[i, j]) >= 0:
                 mask[i, j] = 1
-    return mask
+    return mask          
 
-def LRC_filling_1(input, mask):
-    assert input.shape == mask.shape
-    h, w = input.shape
-    output = np.copy(input)
-    count = 0
-    for i in range(h):
-        for j in range(0, w):
-            if mask[i, j] == 0 and j > 0 and j < (w - 1):
-                temp_l = (mask[i, 0:j][::-1] != 0).argmax()
-                temp_r = (mask[i, (j+1):] != 0).argmax()
-                if temp_l > 0 and temp_r > 0:
-                    count += 1
-                    output[i, j] = min(input[i, j + temp_r + 1], input[i, j - temp_l - 1])
-                    print('get')
-                elif temp_l > 0 and temp_r == 0:
-                    output[i, j] = input[i, j - temp_l - 1]
-                elif temp_l == 0 and temp_r > 0:
-                    output[i, j] = input[i, j + temp_r + 1]
-    print(count)
-    return output
-
-def LRC_filling_2(input, mask):
-    assert input.shape == mask.shape
-    h, w = input.shape
-    output = np.copy(input)
-
-    for i in range(h):
-        for j in range(w):
-            if mask[i, j] == 0 and j > 0 and j < (w - 1):
-                flag_l = 0
-                flag_r = 0
-                for k in range(1, w):
-                    if mask[i, j - k] == 1 and flag_l == 0 and (j - k) >= 0:
-                        temp_l = input[i, j - k]
-                        flag_l = 1
-                    if flag_r == 0 and (j + k - w) < 0:
-                        if mask[i, j + k] == 1:
-                            temp_r = input[i, j + k]
-                            flag_r = 1
-                    if flag_l == 1 and flag_r == 1:
-                        output[i, j] = min(temp_l, temp_r)
-                        # output[i, j] = 1
-                        break
-    return output
-
-def LRC_filling_3(input, mask):
+def LRC_filling(input, mask):
     assert input.shape == mask.shape
     h, w = input.shape
     output = np.copy(input)
@@ -82,16 +36,9 @@ def LRC_filling_3(input, mask):
     return output
 
 if __name__ == '__main__':
-    img_root_1 = '/media/home_bak/swap/Robot_depth_completion/depth_completion-master/workspace/exp_stereo_lidar/val_output_1_epoch_11/0000000020.png'
-    img_root_2 = '/media/home_bak/swap/Robot_depth_completion/depth_completion-master/workspace/exp_stereo_lidar/val_output_2_epoch_11/0000000020.png'
-    # img_root_3 = '/home/ding/下载/teddy/disp2.png'
-    # gt = cv2.imread(img_root_3)
-    # gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY)
-    # gt = np.array(gt)
-
-    # print(np.mean(gt), gt.shape)
-    # disp_1 = SAD_2(img_root_1, img_root_2, 5, 30, K=0)
-    # disp_2 = SAD_2(img_root_1, img_root_2, 5, 30, K=1)
+    img_root_1 = ''
+    img_root_2 = ''
+   
     disp_1 = cv2.imread(img_root_1)
     disp_1 = cv2.cvtColor(disp_1, cv2.COLOR_BGR2GRAY)
     disp_1 = np.array(np.array(disp_1) / 4, dtype=int)
@@ -107,13 +54,12 @@ if __name__ == '__main__':
 
     '''start_time = time.time()
     for i in range(20):
-        img_filled = LRC_filling_3(disp_1, mask)
+        img_filled = LRC_filling(disp_1, mask)
         print('get')
     print((time.time() - start_time) / 20)
     assert False'''
-    FULL_KERNEL_5 = np.ones((5, 5), np.uint8)
-    # img_filled = cv2.dilate(np.array(disp_1 * mask, dtype=np.float32), FULL_KERNEL_5)
-    img_filled = LRC_filling_3(disp_1, mask)
+    
+    img_filled = LRC_filling(disp_1, mask)
     img_filled = np.array(img_filled, dtype=np.float32)
     img_filter = cv2.medianBlur(img_filled, 5)
 
